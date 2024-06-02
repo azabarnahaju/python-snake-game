6bab1cec65c4c8229994a9028b64ebf4b6d306d5
azabarnahaju <kanikalexandra@gmail.com>
2024-06-02 17:35:08 +0200

    Connect snake, food and score logic in main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from score import Score
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,11 +10,14 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Score()
 
 is_game_on = True
 while is_game_on:
     screen.update()
     screen.listen()
+    score.scoreboard()
 
     screen.onkey(key='a', fun=snake.go_left)
     screen.onkey(key='s', fun=snake.go_down)
@@ -21,4 +26,13 @@ while is_game_on:
 
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.create_new_food()
+        score.increment()
+        snake.grow()
+
+    if snake.has_bit_itself() or snake.has_hit_wall():
+        is_game_on = False
+
+score.game_over()
 screen.exitonclick()
