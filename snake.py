2ff0cf1a7b05422912d8f808b153a2f9c814d684
azabarnahaju <kanikalexandra@gmail.com>
2024-06-02 16:09:32 +0200

    Refactor snake movement

--- snake.py
+++ snake.py
@@ -6,7 +6,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-        self.curr_heading = 0
+        self.head = self.segments[0]
 
     def create_snake(self):
         x, y = 20, 0
@@ -19,7 +19,6 @@ class Snake:
             self.segments.append(t)
 
     def move(self):
-        self.curr_heading = self.segments[0].heading()
         time.sleep(0.1)
 
         for i in range(len(self.segments)-1, 0, -1):
@@ -28,29 +27,17 @@ class Snake:
         self.segments[0].fd(20)
 
     def go_left(self):
-        match self.curr_heading:
-            case 90:
-                self.segments[0].lt(90)
-            case 270:
-                self.segments[0].rt(90)
+        if self.head.heading() != 0:
+            self.head.setheading(180)
 
     def go_right(self):
-        match self.curr_heading:
-            case 90:
-                self.segments[0].rt(90)
-            case 270:
-                self.segments[0].lt(90)
+        if self.head.heading() != 180:
+            self.head.setheading(0)
 
     def go_up(self):
-        match self.curr_heading:
-            case 0:
-                self.segments[0].lt(90)
-            case 180:
-                self.segments[0].rt(90)
+        if self.head.heading() != 270:
+            self.head.setheading(90)
 
     def go_down(self):
-        match self.curr_heading:
-            case 180:
-                self.segments[0].lt(90)
-            case 0:
-                self.segments[0].rt(90)
+        if self.head.heading() != 90:
+            self.head.setheading(270)
